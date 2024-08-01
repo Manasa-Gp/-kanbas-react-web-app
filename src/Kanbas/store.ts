fe368851a9a0c3c78ffe16657d5c4d866eafaa7f
Manasa-Gp <manasa.g.pavushetty@gmail.com>
2024-08-01 15:43:54 -0400

    quiz

--- src/Kanbas/store.ts
+++ src/Kanbas/store.ts
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import modulesReducer from "./Courses/Modules/reducer";
 import assignmentsReducer from "./Courses/Assignments/reducer";
-import quizzesReducer from "./Courses/Quizzes/reducer"; 
+import quizzesReducer from "./Courses/Quizzes/reducer";
 const store = configureStore({
   reducer: {
     modulesReducer,
