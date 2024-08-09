812807db0ce897e1d140d4f7b3c4ef04b86bdae5
SDK <50303777+sdk-bens@users.noreply.github.com>
2024-08-09 14:37:04 -0400

    Added student  dashborad, and  functionality for student and faculty

---
+++ src/Kanbas/Student Dashboard/index.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+export default function StudentDashboard({
+  course,
+  courses,
+}: {
+  course: any;
+  courses: any[];
+}) 
+{
+  return (
+    <div className="p-4" id="student-dashboard">
+      <h1 id="student-dashboard-title">Student Dashboard</h1> 
+      <hr />
+      <h2 id="student-dashboard-published">
+        My Courses ({courses.length})
+      </h2> 
+      <hr />
+      <div className="row" id="student-dashboard-courses">
+        <div className="row row-cols-1 row-cols-md-5 g-4">
+          {courses.map((course) => (
+            <div key={course.id} className="col" style={{ width: "300px" }}>
+              <Link
+                to={`/Kanbas/Courses/${course.id}/Home`}
+                className="text-decoration-none"
+              >
+                <div className="card rounded-3 overflow-hidden">
+                  <img src={`${course.images}`} height="190" />
+
+                  <div className="card-body">
+                    <span
+                      className="wd-dashboard-course-link"
+                      style={{
+                        textDecoration: "none",
+                        color: "navy",
+                        fontWeight: "bold",
+                      }}
+                    >
+                      {course.name}
+                    </span>
+                    <p
+                      className="wd-dashboard-course-title card-text"
+                      style={{ maxHeight: 53, overflow: "hidden" }}
+                    >
+                      {course.description}
+                    </p>
+                    <Link
+                      to={`/Kanbas/Courses/${course.id}/Home`}
+                      className="btn btn-primary"
+                    >
+                      Go
+                    </Link>
+                  </div>
+                </div>
+              </Link>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -4,116 +4,111 @@ import "./styles.css";
 import { Routes, Route, Navigate } from "react-router";
 import Courses from "./Courses";
 import * as client from "./Courses/client";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import store from "./store";
 import { Provider } from "react-redux";
 import Account from "./Account";
 import ProtectedRoute from "./Account/ProtectedRoute";
 import * as profile_client from "./Account/client";
-
+import StudentDashboard from "./Student Dashboard";
 
 export default function Kanbas() {
   const [courses, setCourses] = useState<any[]>([]);
   const [profile, setProfile] = useState<any>({});
-
-
 
   const fetchProfile = async () => {
     try {
       const account = await profile_client.profile();
       setProfile(account);
     } catch (err: any) {
-      console.log("Profiler error: react")
+      console.log("Profile error: react");
     }
   };
- 
- 
 
   const fetchCourses = async () => {
-    // const courses = await client.fetchAllCourses();
-    
     const courses_ids = await profile_client.getUserEnrollments(profile.username);
-    console.log("*****************************************")
-    console.log("PROFILE USER NAME",profile.username );
-    console.log("COURSES IDS",courses_ids );
-    console.log("*****************************************")
-   
+    console.log("*****************************************");
+    console.log("PROFILE USER NAME", profile.username);
+    console.log("COURSES IDS", courses_ids);
+    console.log("*****************************************");
+    
     const filtered_courses = client.fetchCoursesByIds(courses_ids);
-
-
-    setCourses( await filtered_courses);
-
-   
+    setCourses(await filtered_courses);
   };
+
   useEffect(() => {
     fetchProfile();
-    console.log("PROFILE USER NAME",profile.username );
     if (profile.username) {
       fetchCourses();
-     
     }
-   
-  }, []);
+  }, [profile.username]);
 
   const [course, setCourse] = useState<any>({
-     name: "New Course", number: "New Number",
+    name: "New Course", number: "New Number",
     startDate: "2023-09-10", endDate: "2023-12-15", description: "New Description",
   });
+
   const addNewCourse = async () => {
     const newCourse = await client.createCourse(course);
-    setCourses([ ...courses, newCourse ]);
+    setCourses([...courses, newCourse]);
   };
 
   const deleteCourse = async (courseId: string) => {
     await client.deleteCourse(courseId);
-    setCourses(courses.filter(
-      (c) => c._id !== courseId));
+    setCourses(courses.filter((c) => c._id !== courseId));
   };
 
   const updateCourse = async () => {
     await client.updateCourse(course);
-    setCourses(
-      courses.map((c) => {
-        if (c._id === course._id) {
-          return course;
-        } else {
-          return c;
-        }
-      })
-    );
+    setCourses(courses.map((c) => (c._id === course._id ? course : c)));
   };
-
 
   return (
     <Provider store={store}>
-  <div id="wd-kanbas" className="h-100">
-  <div className="d-flex h-100">
-    <div className="d-none d-md-block bg-black" style = {{marginRight:"25px"}}>
-      <KanbasNavigation />
-    </div>
-    <div className="flex-fill">
-      <Routes>
-      <Route path="/" element={<Navigate to="Dashboard" />} />
-      <Route path="/Account/*" element={<Account />} />
-      <Route path="Dashboard" element={
-                        <ProtectedRoute>
-            <Dashboard
-              courses={courses}
-              course={course}
-              setCourse={setCourse}
-              addNewCourse={addNewCourse}
-              deleteCourse={deleteCourse}
-              updateCourse={updateCourse}/>
-                            </ProtectedRoute>
-              } />
-            <Route path="Courses/:cid/*" element={<ProtectedRoute><Courses courses={courses} /> </ProtectedRoute>} />
-            <Route path="Calendar" element={<h1>Calendar</h1>} />
-            <Route path="Inbox" element={<h1>Inbox</h1>} />
-      </Routes>
-    </div>
-  </div>
-</div>
-</Provider>
-);}
-
-
+      <div id="wd-kanbas" className="h-100">
+        <div className="d-flex h-100">
+          <div className="d-none d-md-block bg-black" style={{ marginRight: "25px" }}>
+            <KanbasNavigation />
+          </div>
+          <div className="flex-fill">
+            <Routes>
+              <Route path="/" element={<Navigate to="Dashboard" />} />
+              <Route path="/Account/*" element={<Account />} />
+              {profile.role === "FACULTY" ? (
+                <>
+                  <Route path="Dashboard" element={
+                    <ProtectedRoute>
+                      <Dashboard
+                        courses={courses}
+                        course={course}
+                        setCourse={setCourse}
+                        addNewCourse={addNewCourse}
+                        deleteCourse={deleteCourse}
+                        updateCourse={updateCourse}
+                      />
+                    </ProtectedRoute>
+                  } />
+                  <Route path="Courses/:cid/*" element={<ProtectedRoute><Courses courses={courses} /></ProtectedRoute>} />
+                </>
+              ) : profile.role === "STUDENT" ? (
+                <>
+                  <Route path="Dashboard" element={
+                    <ProtectedRoute>
+                      <StudentDashboard
+                        courses={courses}
+                        course={course}
+                      />
+                    </ProtectedRoute>
+                  } />
+                  <Route path="Courses/:cid/*" element={<ProtectedRoute><Courses courses={courses} /></ProtectedRoute>} />
+                </>
+              ) : null}
+              <Route path="Calendar" element={<h1>Calendar</h1>} />
+              <Route path="Inbox" element={<h1>Inbox</h1>} />
+            </Routes>
+          </div>
+        </div>
+      </div>
+    </Provider>
+  );
+}
