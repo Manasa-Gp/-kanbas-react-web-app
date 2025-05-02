7b756665a4e6175171c10cfcf1eb5b402a373caa
Manasa-Gp <manasa.g.pavushetty@gmail.com>
2025-05-02 11:48:47 -0400

    check

--- src/Kanbas/Account/index.tsx
+++ src/Kanbas/Account/index.tsx
@@ -4,53 +4,35 @@ import AccountNavigation from "./Navigation";
 import { useSelector } from "react-redux";
 import Profile from "./Profile";
 import Signup from "./Signup";
-import Session from "./Session"; // Import the Session component
-
 export default function Account() {
   const { currentUser } = useSelector((state: any) => state.accountReducer);
 
   return (
-    <Session> {/* Wrap the routes inside Session to handle session state */}
-      <div>
-        <div className="d-flex">
-          <div className="d-none d-md-block">
-            <AccountNavigation />
-          </div>
-          <div className="flex-fill p-4 pt-0">
-            <Routes>
-              {/* Default Route: If user is logged in, navigate to Profile, else Signin */}
-              <Route
-                path="/"
-                element={
-                  currentUser ? (
-                    <Navigate to="/Kanbas/Account/Profile" />
-                  ) : (
-                    <Navigate to="/Kanbas/Account/Signin" />
-                  )
-                }
-              />
-
-              {/* Signin Route */}
-              <Route
-                path="/signin"
-                element={currentUser ? <Navigate to="/Kanbas/Account/Profile" /> : <Signin />}
-              />
-
-              {/* Signup Route */}
-              <Route
-                path="/signup"
-                element={currentUser ? <Navigate to="/Kanbas/Account/Profile" /> : <Signup />}
-              />
-
-              {/* Protected Profile Route: Only accessible if user is logged in */}
-              <Route
-                path="/profile"
-                element={currentUser ? <Profile /> : <Navigate to="/Kanbas/Account/Signin" />}
-              />
-            </Routes>
-          </div>
+    <div>
+      <div className="d-flex">
+        <div className="d-none d-md-block">
+          <AccountNavigation />
+        </div>
+        <div className="flex-fill p-4 pt-0">
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <Navigate
+                  to={
+                    currentUser
+                      ? "/Kanbas/Account/Profile"
+                      : "/Kanbas/Account/Signin"
+                  }
+                />
+              }
+            />
+            <Route path="/Signin" element={<Signin />} />
+            <Route path="/Signup" element={<Signup />} />
+            <Route path="/Profile" element={<Profile />} />
+          </Routes>
         </div>
       </div>
-    </Session>
+    </div>
   );
 }
